29775d67168f185c6e38fe2a0f42519aaff6a559
MoD <sasina.mod@gmail.com>
2018-01-04 15:51:31 +0700

    test3 commit

--- app/src/main/java/modsasi/yourtoolbox/MainActivity.java
+++ app/src/main/java/modsasi/yourtoolbox/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setTitle("Test 123");
+        setTitle("Test 1234");
     }
 }
